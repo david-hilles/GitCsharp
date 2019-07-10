a931a978db679f06951f9559283ac3e0f9f9b665
Jalin Habei <Jalin.Habei@gmail.com>
2019-07-10 10:56:18 -0300

    arrais de duas posicoes

---
+++ ConsoleArraybidimensional/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleArraybidimensional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] listaDeLivros = new string[2, 2]
+                {{"o pequeno","sim " },{ "O grande","não"} };
+
+            for (int i = 0; i < listaDeLivros.GetLength(0); i++)
+            {
+                var Nomedolivro = listaDeLivros[i, 0];
+                var disponivel = listaDeLivros[i, 1];
+
+                Console.WriteLine($"Nome do livro:{Nomedolivro} disponivel:{disponivel}.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
