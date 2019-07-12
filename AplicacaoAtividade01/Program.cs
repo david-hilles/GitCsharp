92b71a852b0b224351dc02367d096f908ea9ad45
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-12 14:15:56 -0300

    Atividade01 maior de 18

---
+++ AplicacaoAtividade01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacaoAtividade01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Seja BemVindo");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Sistema que indica se a pessoa é maior de 18 anos");
+            Console.WriteLine("Digite a Idade");
+            Console.WriteLine("-------------------------------------------------");
+
+            int.TryParse(Console.ReadLine(), out int Idade);
+
+            if (Idade < 18)
+            {
+                Console.WriteLine("Proibito tomar bebeidas alcoolicas");
+            }
+            else
+            {
+                Console.WriteLine("Você não pode beber");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
