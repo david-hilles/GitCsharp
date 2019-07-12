8854fb913ae01b84fb81aad8cefa4644bc869055
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-12 15:13:26 -0300

    Atividade02Lista de Bolo

---
+++ Atividade02.Lista de umbolo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade02.Lista_de_umbolo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ReceitaDeBolo
+            Console.WriteLine("__________________________________");
+            Console.WriteLine("      Receita completa Bolo ");
+            Console.WriteLine("__________________________________");
+
+            Console.ReadKey();
+
+            string[] receitadebolo = new string[6] { "2 copos de açúcar",
+                                                     "3 ovos",
+                                                     "1 copo de leite",
+                                                     "2 colheres de margarina",
+                                                     "1 colher (chá) de fermento em pó",
+                                                     "2 copos de farinha de trigo",};
+
+             for (int i = 0; i < receitadebolo.Length; i++)
+                Console.WriteLine(receitadebolo[i]);
+
+                Console.ReadKey();
+
+            string[] mododepreparo = new string[4] { "Junte todos os ingredientes no copo do liquidificador.",
+                                                    "Bata até perceber que a mistura ficou homogênea",
+                                                 "Despeje o conteúdo numa fôrma untada e leve para assar em forno aquecido por 35 minutos",
+                                                "Espere esfriar e sirva com calda de chocolate"};
+
+            for(int i = 0; i < mododepreparo.Length; i++)
+            {
+                Console.WriteLine(mododepreparo[i]);
+                Console.ReadKey();
+            }
+             
+  
+
+
+            //{ "3 ovos" },
+            //{ "1 copo de leite."},
+            //{ "2 colheres de margarina."},
+            //{ " 1 colher (chá) de fermento em pó"},
+            //{ "2 copos de farinha de trigo."}
+
+
+
+        }
+    }
+}
---
+++ Atividade02.Lista de umbolo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Atividade02.Lista de umbolo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Atividade02.Lista de umbolo")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("39d31995-f235-44a2-9640-e77f16b9f7b9")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
