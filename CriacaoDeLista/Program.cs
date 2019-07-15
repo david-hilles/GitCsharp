474f8619c15d5e8cff7a9b7ebbe83bbb22919051
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-15 08:41:37 -0300

    Uma forma facil mostrar uma coleção

---
+++ CriacaoDeLista/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriacaoDeLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Iniciando uma lista de string com array linear
+            string[] lista = new string[10];           // aqui definimos as posiçoes da lista iniciando ela com 10
+            //espaços na memoria para alocar informaçoes de texto
+
+            //laço que usamos geralmente para andar sobre as posições da nossa lista
+            for (int i = 0; i < lista.Length; i++)
+            {
+                lista[i] = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss.fff");
+
+            }
+            foreach (var item in lista) //"var item" indica uma unicade da nossa lista "in lista"
+                //indica a lista que desejamos varrer
+                Console.WriteLine(item);//Aqui apresentamos essa informacao na tela
+
+            Console.ReadKey();
+
+        }
+    }
+}
