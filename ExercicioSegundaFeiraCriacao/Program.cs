755cb4b6822f37ffb00fa55e036bf94dcb838879
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-15 13:30:09 -0300

    ExercicioSegunda-feira.ListaDeCarros.

---
+++ ExercicioSegundaFeiraCriacao/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioSegundaFeiraCriacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] listaDeCarros = new string[5, 4];
+
+            for (int i = 0; i < listaDeCarros.GetLength(0); i++)
+            {
+
+                if (listaDeCarros[i, 0] != null)
+                    continue;
+
+                Console.WriteLine("\r\nInforme um carro para adicionar na lista");
+                var nomeCarro = Console.ReadLine();
+                int IDLista = 0;
+
+                listaDeCarros[i, 0] = (IDLista++).ToString();
+                listaDeCarros[i, 1] = nomeCarro;
+
+                Console.WriteLine("\r\ninforme a placa do carro para adicionar na lista");
+                var placa = Console.ReadLine();
+
+                listaDeCarros[i, 2] = placa;
+
+                Console.WriteLine("\r\ninforme a cor do carro para adicionar na lista");
+                var cor = Console.ReadLine();
+
+                listaDeCarros[i, 3] = cor;
+
+                Console.WriteLine("Deseja inserir um novo registro? sim(1) Não(0)");
+                var continuar = Console.ReadKey().KeyChar.ToString();
+
+                if (continuar == "0")
+                    break;
+
+
+            }
+
+
+            Console.WriteLine("Registro adicionado com sucesso, lista das informações adicionadas: ");
+
+            for (int i = 0; i < listaDeCarros.GetLength(0); i++)
+                //Utilizamos o string format, basicamente ele faz da mesma forma que o $
+                //a diferença entre eles é que este e um cara em grande quantidades
+                //acaba sendo mais rápido
+
+                Console.WriteLine(string.Format("Registro ID {0} - Carro: {1} - Placa: {2} - Cor: {3} ", listaDeCarros[i, 0], listaDeCarros[i, 1], listaDeCarros[i, 2], listaDeCarros[i, 3]));
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
