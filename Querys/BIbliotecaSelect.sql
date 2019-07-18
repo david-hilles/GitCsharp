574af08ae47c5dee84994be3546302b91afb635c
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-18 14:19:23 -0300

    Tabela RelacionadasBiblioteca

---
+++ Querys/BIbliotecaSelect.sql
@@ -0,0 +1,8 @@
+﻿select                                           
+         BL.Nome,
+         COUNT(LV.Id) as 'QuantLivros',
+         US.Nome as 'UserName'
+ from biblioteca BL
+ inner join Livro LV on BL.Id = LV.biblioteca
+ inner join Usuario US on LV.UserInc = US.Id
+ group by BL.Nome,US.Nome
---
+++ Querys/FOODTRUCK03.sql
@@ -0,0 +1,9 @@
+﻿    select
+
+  TipoDog,
+  TotalDeVendas,
+    MONTH (DataVendas) as 'Mes'
+
+	from FoodTruck
+	group by TipoDog,TotalDeVendas,MONTH (DataVendas)
+	order by MONTH (DataVendas) desc
