50b86c72a0623088ee1ee2b08a4b95a30b32153f
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-19 09:58:58 -0300

    SistemaBiBlioteca/Querys

---
+++ SistemaDeBibliotecaDB/Querys/Creat Autores.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Autores]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY (1, 1), 
+    [Nome] VARCHAR(100) NOT NULL, 
+    [Descricao] VARCHAR(1000) NOT NULL,
+
+)
---
+++ SistemaDeBibliotecaDB/Querys/Creat Generos.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Generos]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY (1, 1), 
+    [Tipo] VARCHAR(200) NOT NULL, 
+    [Descricao] VARCHAR(1000) NOT NULL
+)
