67d86a09d74a4eea66f88b044a914597c782607e
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-19 13:16:02 -0300

    SistemaDeBoletim

---
+++ ExerciciosBancoDeDados/Creat Disciplina.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Aluno] (
+    [Id]         INT           IDENTITY (1, 1) NOT NULL,
+    [Nome]       VARCHAR (100) NOT NULL,
+    [Disciplina] INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Aluno_Disciplina] FOREIGN KEY ([Disciplina]) REFERENCES [dbo].[Disciplina] ([Id])
+);
+
---
+++ ExerciciosBancoDeDados/Creat Notas.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Notas] (
+    [Id]    INT IDENTITY (1, 1) NOT NULL,
+    [Nota1] INT NOT NULL,
+    [Aluno] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Notas_Aluno] FOREIGN KEY ([Aluno]) REFERENCES [dbo].[Aluno] ([Id])
+);
+
