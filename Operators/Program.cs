eb52616276c3291bd14eca59f318408f862ed6f1
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-23 16:57:43 -0300

    PrimeiroContatocomClass

---
+++ Operators/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Passei o Nome, Senha
+
+            var elementoX = new Teste(5,5);
+
+            elementoX.MostrarInformacoes();
+
+            elementoX.AlterarInformacoes(5, 5);
+
+            elementoX.MostrarInformacoes();
+
+            elementoX.AlterarInformacoes();
+
+            elementoX.MostrarInformacoes();
+
+            Console.ReadKey();
+
+        }
+    }
+    public class Teste
+    {
+        private int A { get; set; } = 0;
+        private int B { get; set; } = 0;
+
+        public Teste(int pA,int pB)                      //Preparador
+        {
+            A = pA;
+            B = pB;
+
+        }
+
+        public void AlterarInformacoes()              //Sobrecarregar 
+        {
+            A = B * 100;
+        }
+        public void AlterarInformacoes(int pA, int pB)
+        {
+            A = pA * 2;
+            B = pB * 2;
+
+        }
+        public void MostrarInformacoes()
+        {
+
+            Console.WriteLine(A + B);
+
+        }
+    }
+}
