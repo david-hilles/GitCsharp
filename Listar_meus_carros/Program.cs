0ce054396ec8a265e0b65f8c94cd4360b6c840cd
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-29 17:23:39 -0300

    Exercicio.29/07/2019. ListasMeusCarros

---
+++ Listar_meus_carros/Program.cs
@@ -0,0 +1,65 @@
+﻿using Listar_meus_carros.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listar_meus_carros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Carros> listacarros = new List<Carros>();
+
+            var opcaoMenu = MenuInicial();
+
+
+
+
+        }
+        public static void AdicionarVeiculo(List<Carros> listacarros)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                listacarros.Add(new Carros()
+                {
+                    Modelo = RetornaValores("Modelo"),
+                    Ano = int.Parse(RetornaValores("Ano")),
+                    Placa = RetornaValores("Placa"),
+                    Cv = int.Parse(RetornaValores("Cv"))
+
+                });
+            }
+            ListarCarros(listacarros);
+
+        }
+
+        public static void ListarCarros(List<Carros> listacarros)
+        {
+            foreach (Carros item in listacarros)
+                Console.WriteLine($"Modelo: {item.Modelo} Ano : {item.Ano} Placa :{item.Placa} Cv : {item.Cv}");
+            Console.ReadKey();
+        }
+
+        public static string RetornaValores(string modelo)
+        {
+            Console.WriteLine($"Informe o valor para o campo:{modelo}");
+            return Console.ReadLine();
+        }
+        public static int MenuInicial()
+        {
+
+            Console.WriteLine("Menu - Inicial");
+            Console.WriteLine("1 - Adicionar Carrros.");
+            Console.WriteLine("2 - Listar Carros.");
+            Console.WriteLine("3 - Sair.");
+
+            int.TryParse(Console.ReadKey().KeyChar.ToString(), out int opcao);
+            return opcao;
+        }
+
+
+    }
+}
---
+++ MInhaPrimeiraListaTipada/Program.cs
@@ -0,0 +1,83 @@
+﻿using MInhaPrimeiraListaTipada.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MInhaPrimeiraListaTipada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Lanche> minhaLista = new List<Lanche>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Insira os Dados : ");
+                minhaLista.Add(new Lanche()
+                {
+                    Nome = RetornaValores("Nome"),
+                    Quantidade = int.Parse(RetornaValores("Quantidade")), //out int Quantidade,
+                    Valor = double.Parse(RetornaValores("Valor")) 
+                });
+
+
+            }
+
+         //  minhaLista.Add(new Lanche()
+         //  {
+         //      Nome = "Pao de queijo",
+         //      Quantidade = 9,
+         //      Valor = 1.85
+         //  });
+         //
+         //  minhaLista.Add(new Lanche()
+         //  {
+         //      Nome = "Bolinho de Carne + Refri Bonus : Gastrite",
+         //      Quantidade = 2,
+         //      Valor = 7.85
+         //  });
+         //  minhaLista.Add(new Lanche()
+         //  {
+         //      Nome = "Pastel de frango",
+         //      Quantidade = 2,
+         //      Valor = 7.85
+         //  });
+         //  minhaLista.Add(new Lanche()
+         //  {
+         //      Nome = "X salada",
+         //      Quantidade = 4,
+         //      Valor = 7.85
+         //  });
+            foreach (Lanche item in minhaLista)
+                Console.WriteLine($"Lanches disponiveis: {item.Nome}");
+
+         //   Console.WriteLine("Removendo Item");
+            // minhaLista.Remove(minhaLista.FirstOrDefault(x => x.Quantidade == 2));
+        //  
+        //   foreach (Lanche item in minhaLista) 
+        //   {
+        //       if (item.Quantidade == 2)
+        //          
+        //       {
+        //           minhaLista.Remove(item);
+        //           break;
+        //       }
+        //
+        //   }
+        //
+        //   foreach (Lanche item in minhaLista)
+        //       Console.WriteLine($"Lanches disponiveis: {item.Nome}");
+
+            Console.ReadKey();
+
+        }
+        public static string RetornaValores (string nome)
+        {
+            Console.WriteLine($"Informe o valor para o campo:{nome}");
+            return Console.ReadLine();//
+        }
+    }
+}
